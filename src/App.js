7f71b2adb1aff0dfadf5dd4bdefb1e159dc7952b
shmgillani <99495656+shmgillani@users.noreply.github.com>
2023-01-27 16:31:21 +0500

    project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import AllUsers from "./Components/AllUsers";
+import AddUser from "./Components/AddUser";
+import EditUser from "./Components/EditUser";
+import NotFound from "./Components/NotFound";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/all" component={AllUsers} exact />
+        <Route path="/add" component={AddUser} exact />
+        <Route path="/edit/:id" component={EditUser} exact />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
